50471f9d4f46133b7d0d1613432f772a7bcaceae
davi9533 <38536291+davi9533@users.noreply.github.com>
2018-04-19 22:12:51 +0530

    Initial commit

---
+++ Prog1/exe_2.java
@@ -0,0 +1,21 @@
+package Prog1;
+
+public class exe_2 {
+	int a=20;
+	int b=5;
+	 
+	public static void main(String[] args) {
+	
+		exe_2 x= new exe_2();
+		
+		System.out.println("Sum: "+(x.a+x.b));
+		System.out.println("Difference: "+(x.a-x.b));
+		System.out.println("Quotient: "+(x.a/x.b));
+		System.out.println("Product: "+(x.a*x.b));
+		System.out.println("Remainder: "+(x.a%x.b));
+		
+		}
+	}
+	
+
+
